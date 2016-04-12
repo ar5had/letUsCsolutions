b70911d18156db825e482c48b3de9f29241b02b2
arshad <arshdkhn1@gmail.com>
2016-04-12 23:53:03 +0530

    Indentation

--- towerOfHanoi.c
+++ towerOfHanoi.c
@@ -2,33 +2,36 @@
 
 void moveTower(int n, char source[15], char destination[15] , char temporary[15]){
 	
-	
-	
 	if( n > 0){
 		
 		moveTower(n-1,source,temporary,destination);
-		printf("Step -Move ring from %s to %s \n",source,destination);
-		moveTower(n-1,temporary,destination,source);
-		
-		
-	}	
 
+		printf("Step -Move ring from %s to %s \n",source,destination);
+		
+		moveTower(n-1,temporary,destination,source);
+			
+	}	
 	
 }
-
-
 
 int main(){
 
 	int rings;
 	
 	printf(" This program tells solution of tower of hanoi for n-rings \n");
+	
 	printf(" Enter the n-rings: ");
+
 	scanf("%d",&rings);
+
 	if(rings<1){
+
 		printf("Bad Value of rings inputed by user!");
+
 		exit(12);
+
 	}
+
 	moveTower(rings,"source","destination","temporary");
 
 	return 0;
